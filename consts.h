a148f6ec832e49852372b489222762544d76d999
Illya Kobzev <technomag82@gmail.com>
2023-09-06 11:18:34 +0300

    Updated config of package
     - updated README

--- consts.h
+++ consts.h
@@ -1,7 +1,7 @@
 #ifndef CONSTS_H
 #define CONSTS_H
 
-const QString PROGRAM_NAME = "URLCollector v2.0";
+const QString PROGRAM_NAME = "URLCollector v2.1";
 const QString PROGRAM_DIR = "/.urlcol";
 const QString PROGRAM_CONFIG = "/.urlcol/url.config";
 const QString LOCK_FILE = "/.urlcol/url.lock";
